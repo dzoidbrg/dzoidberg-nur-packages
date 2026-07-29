f20fa10f263770bd3c1a5d98f5dbbd961a7604fa
Your Name <you@example.com>
2026-07-30 00:17:32 +0200

    All good?

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@
   # flakeModules = { }; # flake-parts modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  octarine-bin = pkgs.callPackage ./pkgs/octarine-bin { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
---
+++ pkgs/octarine-bin/default.nix
@@ -0,0 +1,51 @@
+pkgs @ { stdenv }:
+
+stdenv.mkDerivation rec {
+  pname = "octarine";
+  version = "0.48.1";
+
+  src = pkgs.fetchurl {
+    url = "https://pub-3d35bc018fc54f11bde129e3e73e8002.r2.dev/${version}/linux/Octarine-bin-${version}-1-x86_64.pkg.tar.zst";
+    hash = "sha256-7nZry+rsRiXDlsigTeIbvaD0ODcbLHbpl4h6ba4KFyA=";
+  };
+
+  nativeBuildInputs = with pkgs; [ zstd autoPatchelfHook makeWrapper ];
+
+  buildInputs = with pkgs; [
+    openssl
+    glib
+    gtk3
+    libsoup_3
+    webkitgtk_4_1
+  ];
+  runtimeDependencies = with pkgs; [
+    libappindicator
+     # not detected by autopatchelf, so specified here
+  ];
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm755 usr/bin/octarine "$out/bin/octarine"
+    
+    install -Dm644 usr/share/icons/hicolor/128x128/apps/octarine.png "$out/share/icons/hicolor/128x128/apps/octarine.png"
+    install -Dm644 usr/share/icons/hicolor/256x256@2/apps/octarine.png "$out/share/icons/hicolor/256x265/apps/octarine.png"
+    install -Dm644 usr/share/icons/hicolor/32x32/apps/octarine.png "$out/share/icons/hicolor/32x32/apps/octarine.png"
+
+    install -Dm644 usr/share/applications/Octarine.desktop "$out/share/applications/Octarine.desktop"
+
+
+    runHook postInstall
+  '';
+  postFixup = ''
+    wrapProgram $out/bin/octarine $wrapperfile \
+     --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.desktop-file-utils ]}
+  ''; # https://github.com/tauri-apps/plugins-workspace/issues/2922 
+  meta = with pkgs.lib; {
+    homepage = "https://octarine.app/";
+    description = "Octarine Editor";
+    mainProgram = "octarine";
+    platforms = platforms.linux;
+  };
+}
